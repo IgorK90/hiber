519c7336bae235e1ca59c09f987fd65bbaf15786
Home <igor.kiyashko@inbox.ru>
2022-08-17 19:41:18 +0600

    insert

--- src/main/java/com/example/hiber/Test1.java
+++ src/main/java/com/example/hiber/Test1.java
@@ -7,14 +7,15 @@ import org.hibernate.cfg.Configuration;
 
 public class Test1 {
     public static void main(String[] args) {
-        SessionFactory factory= new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(hiber.entity.Employee.class).buildSessionFactory();
+        SessionFactory factory= new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(com.example.hiber.entity.Employee.class).buildSessionFactory();
         try {
             Session session = factory.getCurrentSession();
-            hiber.entity.Employee emp = new hiber.entity.Employee("Igor", "Kiyashko", "IT", 500);
+            com.example.hiber.entity.Employee emp = new com.example.hiber.entity.Employee("Alex", "Smirnov", "HR", 750);
             session.beginTransaction();
 
             session.save(emp);
             session.getTransaction().commit();
+            System.out.println("DONE");
 
         } catch(Exception e)
         {}
