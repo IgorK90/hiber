542adcff7921e2e84d84d7369f17264a46ec25eb
Home <igor.kiyashko@inbox.ru>
2022-09-02 12:17:21 +0600

    insert

--- src/main/java/hibernate_many_to_many/entity/Child.java
+++ src/main/java/hibernate_many_to_many/entity/Child.java
@@ -19,7 +19,10 @@ public class Child {
     @Column(name = "age")
     private int age;
 
-    @ManyToMany(cascade = CascadeType.ALL)
+    @ManyToMany(cascade = {CascadeType.PERSIST,
+            CascadeType.MERGE,
+            CascadeType.REFRESH,
+            CascadeType.DETACH})
     @JoinTable(
             name = "child_section",
             joinColumns = @JoinColumn(name = "child_id"),
